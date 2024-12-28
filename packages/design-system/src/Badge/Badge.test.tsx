421b003314ce638be08b93605bb92d8d1dbb2acc
thewanionly <pelloani@gmail.com>
2024-12-28 16:28:28 +0800

    test(design-system): use stories in testing file

--- packages/design-system/src/Badge/Badge.test.tsx
+++ packages/design-system/src/Badge/Badge.test.tsx
@@ -1,12 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-import { Badge } from './Badge.tsx';
+import { composeStories } from '@storybook/react';
+import * as stories from './Badge.stories.tsx';
+const { Primary } = composeStories(stories);
 
 it('displays Badge component', async () => {
   // ARRANGE
   const value = '27';
-  render(<Badge value={value} />);
+  render(<Primary value={value} />);
 
   // ACT
 
