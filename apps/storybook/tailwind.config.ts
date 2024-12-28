e2ef55cb4be69a933982f3dfaddc852e5c1ac5c4
thewanionly <pelloani@gmail.com>
2024-12-29 04:48:22 +0800

    fix(apps/storybook): add tsx files in design-system so changes in tailwind in dev mode is reflected
    Ref: https://stackoverflow.com/a/76455741/9608615

--- apps/storybook/tailwind.config.ts
+++ apps/storybook/tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 import sharedConfig from '@repo/tailwind-config';
 
 const config: Pick<Config, 'content' | 'presets'> = {
-  content: ['./index.html'],
+  content: ['./index.html', '../../packages/design-system/src/**/*.{ts,tsx,html,stories.tsx}'],
   presets: [sharedConfig]
 };
 
