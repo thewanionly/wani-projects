b599f5ef318d39a1123525a5d5bc522e28f60ffa
thewanionly <pelloani@gmail.com>
2024-12-29 22:53:21 +0800

    feat(design-system): add size prop and BadgeSize enum (not yet usable)

--- packages/design-system/src/Badge/Badge.tsx
+++ packages/design-system/src/Badge/Badge.tsx
@@ -2,11 +2,18 @@ import { ReactNode } from 'react';
 
 import '../styles/global.css';
 
+enum BadgeSize {
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large'
+}
+
 export interface BadgeProps {
+  size?: BadgeSize;
   children: ReactNode;
 }
 
-export const Badge = ({ children }: BadgeProps) => {
+export const Badge = ({ size = BadgeSize.Medium, children }: BadgeProps) => {
   return (
     <span className='text-sm leading-5 text-neutral-600 border border-neutral-200 rounded-full py-0.5 px-2'>
       {children}
