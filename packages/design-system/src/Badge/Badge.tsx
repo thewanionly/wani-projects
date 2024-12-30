4a75bb27e124eab74c3761d2726ecce836ada23d
thewanionly <pelloani@gmail.com>
2024-12-31 05:14:51 +0800

    feat(packages/design-system): add BadgeVariant enum (not yet supported)

--- packages/design-system/src/Badge/Badge.tsx
+++ packages/design-system/src/Badge/Badge.tsx
@@ -8,12 +8,25 @@ enum BadgeSize {
   Large = 'large'
 }
 
+enum BadgeVariant {
+  Neutral = 'neutral',
+  Error = 'error',
+  Warning = 'warning',
+  Successs = 'success',
+  Brand = 'brand'
+}
+
 export interface BadgeProps {
   size?: BadgeSize;
+  variant?: BadgeVariant;
   children: ReactNode;
 }
 
-export const Badge = ({ size = BadgeSize.Medium, children }: BadgeProps) => {
+export const Badge = ({
+  size = BadgeSize.Medium,
+  variant = BadgeVariant.Neutral,
+  children
+}: BadgeProps) => {
   return (
     <span className='text-sm leading-5 text-neutral-600 border border-neutral-200 rounded-full py-0.5 px-2'>
       {children}
