03b6f2eb0235867cf9a86dc2a079f2cc00e37fc1
thewanionly <pelloani@gmail.com>
2025-01-08 04:41:33 +0800

    feat(packages/design-system): initial styling to Button component

--- packages/design-system/src/Button/Button.tsx
+++ packages/design-system/src/Button/Button.tsx
@@ -1,11 +1,17 @@
+import { cn } from '../utils/styling.ts';
+
 export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   className?: string;
   label?: string;
 }
 
-export const Button = ({ label, ...props }: ButtonProps) => {
+export const Button = ({ className, label, ...props }: ButtonProps) => {
   return (
-    <button type="button" {...props}>
+    <button
+      type="button"
+      className={cn('rounded bg-indigo-700 px-3.5 py-2.5 text-sm text-white', className)}
+      {...props}
+    >
       {label}
     </button>
   );
