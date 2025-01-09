aa2d34a43837a2e791f87934c725b60b30d147fc
thewanionly <pelloani@gmail.com>
2025-01-10 04:51:32 +0800

    feat(packages/design-system): add disabled button styling

--- packages/design-system/src/Button/Button.tsx
+++ packages/design-system/src/Button/Button.tsx
@@ -10,7 +10,9 @@ export const Button = ({ className, label, ...props }: ButtonProps) => {
     <button
       type="button"
       className={cn(
-        'rounded bg-indigo-700 px-3.5 py-2.5 text-sm text-white shadow hover:bg-indigo-800 focus:bg-indigo-800 focus:shadow-buttonFocusRing focus-visible:outline-none motion-safe:transition-colors',
+        'rounded bg-indigo-700 px-3.5 py-2.5 text-sm font-medium text-white shadow',
+        'hover:bg-indigo-800 focus:bg-indigo-800 focus:shadow-buttonFocusRing focus-visible:outline-none motion-safe:transition-colors',
+        'disabled:bg-neutral-100 disabled:text-neutral-400 disabled:shadow-none',
         className
       )}
       {...props}
