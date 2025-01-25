da50ad512871474c607c409abad001ab3e668f05
thewanionly <pelloani@gmail.com>
2025-01-25 21:50:44 +0800

    feat(packages/design-system): supprt for showing char count

--- packages/design-system/src/TextArea/TextArea.test.tsx
+++ packages/design-system/src/TextArea/TextArea.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 import { composeStories } from '@storybook/react';
 
 import * as stories from './TextArea.stories.tsx';
-
-const { Normal, Disabled, Error } = composeStories(stories);
+import { userEvent } from '@testing-library/user-event';
+const { Normal, Filled, Disabled, Error } = composeStories(stories);
 
 describe('TextArea', () => {
   it(`displays textarea's label`, async () => {
@@ -37,6 +37,44 @@ describe('TextArea', () => {
     expect(textAreaByPlaceholder).toBeInTheDocument();
   });
 
+  it(`displays char limit`, async () => {
+    render(<Normal />);
+
+    const charLimit = screen.getByTestId('char-limit');
+    expect(charLimit.textContent).toBe(`${Normal.args.maxLength}`);
+  });
+
+  it(`displays zero char count when there's no inputted text`, async () => {
+    render(<Normal />);
+
+    const charCount = screen.getByTestId('char-count');
+    expect(charCount.textContent).toBe('0');
+  });
+
+  it(`displays char count of the inputted text`, async () => {
+    render(<Filled />);
+
+    const charCount = screen.getByTestId('char-count');
+    expect(charCount.textContent).toBe(`${(Filled.args.defaultValue as string)?.length ?? ''}`);
+  });
+
+  it(`updates the char count when user updates the text input`, async () => {
+    render(<Normal />);
+
+    // initial
+    const charCountInitial = screen.getByTestId('char-count');
+    expect(charCountInitial.textContent).toBe('0');
+
+    // input to the text area
+    const inputValue = 'Hello world';
+    const textArea = screen.getByRole('textbox');
+    await userEvent.type(textArea, inputValue);
+
+    // assert char count has changed to the length of the inputted text
+    const charCountFinal = screen.getByTestId('char-count');
+    expect(charCountFinal.textContent).toBe(`${inputValue.length}`);
+  });
+
   it('displays a disabled textarea element', async () => {
     render(<Disabled />);
 
@@ -49,5 +87,13 @@ describe('TextArea', () => {
 
     const errorMessage = screen.getByText(Error.args.errorMessage as string);
     expect(errorMessage).toBeInTheDocument();
+  });
+
+  xit(`displays error message when current char count exceeds char limit`, async () => {
+    // TODO:
+    render(<Normal />);
+
+    const textArea = screen.getByRole('textbox');
+    expect(textArea).toBeDisabled();
   });
 });
--- packages/design-system/src/TextArea/TextArea.tsx
+++ packages/design-system/src/TextArea/TextArea.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import { ChangeEvent, forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
 import { cn } from '../utils/styling.ts';
 
 export interface TextAreaProps extends React.ComponentProps<'textarea'> {
@@ -7,7 +7,38 @@ export interface TextAreaProps extends React.ComponentProps<'textarea'> {
 }
 
 const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
-  ({ label, id, disabled, errorMessage, ...props }, ref) => {
+  (
+    { label, id, disabled, errorMessage, maxLength, value, defaultValue, onChange, ...props },
+    ref
+  ) => {
+    const innerRef = useRef<HTMLTextAreaElement>(null);
+    const [charCount, setCharCount] = useState(() => {
+      // handles both controlled (`value`) and uncontrolled (`defaultValue`) inputs
+      if (typeof value === 'string' && value) return value.length;
+
+      if (typeof defaultValue === 'string' && defaultValue) return defaultValue.length;
+
+      return 0;
+    });
+
+    // The exposed `ref` will now contain the `innerRef.current` we declared inside this component
+    useImperativeHandle(ref, () => innerRef.current!);
+
+    const handleOnChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+      // update charCount state on every change
+      setCharCount(event.target.value.length);
+
+      // call onChange prop with the same event
+      onChange?.(event);
+    };
+
+    // update charCount state on first load
+    useEffect(() => {
+      if (!innerRef.current) return;
+
+      setCharCount(innerRef.current.value.length);
+    }, []);
+
     return (
       <div className="flex flex-col gap-1.5">
         {label && (
@@ -27,11 +58,19 @@ const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
           )}
           id={id}
           disabled={disabled}
+          maxLength={maxLength}
+          value={value}
+          defaultValue={defaultValue}
+          onChange={handleOnChange}
           {...props}
-          ref={ref}
+          ref={innerRef}
         />
-        <div>
-          {errorMessage && <p className="text-sm font-normal text-red-600">{errorMessage}</p>}
+        <div className="flex justify-between">
+          <p className="text-sm font-normal text-red-600">{errorMessage}</p>
+          <p className="text-right text-sm font-normal text-neutral-500">
+            <span data-testid="char-count">{charCount}</span>/
+            <span data-testid="char-limit">{maxLength}</span>
+          </p>
         </div>
       </div>
     );
